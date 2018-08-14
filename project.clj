f73917befb1d3fdbe501cc9d39e6764ae66400a3
Christopher Brown <io@henrian.com>
2018-08-13 21:24:52 -0500

    Version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fancy/fancy "0.2.2"
+(defproject fancy/fancy "0.2.3"
   :description "Fancy printing -- prettier than pretty"
   :url "https://github.com/chbrown/fancy-clojure"
   :license {:name "Eclipse Public License"
