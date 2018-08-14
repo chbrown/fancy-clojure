a68e06cf978acb5983d2c2004b9343f9fa13bc67
Christopher Brown <io@henrian.com>
2018-08-13 21:15:37 -0500

    Add ansi imports to dev module

--- dev/user.clj
+++ dev/user.clj
@@ -5,6 +5,8 @@
             [clojure.string :as str]
             [clojure.java.io :as io]
 
+            [fancy.ansi :as ansi :refer [bold blink inverse
+                                         black red green blue yellow magenta cyan white]]
             [fancy.layout :as layout :refer [pr-seq pr-pretty prn-pretty]]
             [fancy.string :as string :refer [repeat-to-length pad-start pad-end]]
             [fancy.table :as table :refer [render-table print-table]]
